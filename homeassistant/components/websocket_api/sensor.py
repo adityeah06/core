7c00139995fb27cadeddbad53369a373b15699d5
adityeah06 <147347529+adityeah06@users.noreply.github.com>
2023-10-08 23:33:55 +0200

    Update sensor.py

--- homeassistant/components/websocket_api/sensor.py
+++ homeassistant/components/websocket_api/sensor.py
@@ -2,10 +2,9 @@
 from __future__ import annotations
 
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.core import HomeAssistant, callback
+
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import (
     DATA_CONNECTIONS,
@@ -16,9 +15,8 @@ from .const import (
 
 async def async_setup_platform(
     hass: HomeAssistant,
-    config: ConfigType,
-    async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
+   
+  
 ) -> None:
     """Set up the API streams platform."""
     entity = APICount()
