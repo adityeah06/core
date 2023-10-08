6882900a909451f234c6875f0cfa4f178e0ece47
adityeah06 <147347529+adityeah06@users.noreply.github.com>
2023-10-08 23:50:20 +0200

    Update camera.py

--- homeassistant/components/yi/camera.py
+++ homeassistant/components/yi/camera.py
@@ -53,7 +53,7 @@ async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None,
+ 
 ) -> None:
     """Set up a Yi Camera."""
     async_add_entities([YiCamera(hass, config)], True)
