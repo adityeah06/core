f74bb9dc3cabb64a886e741cc9a59a390933e8ff
adityeah06 <147347529+adityeah06@users.noreply.github.com>
2023-10-08 23:51:04 +0200

    Update application_credentials.py

--- homeassistant/components/youtube/application_credentials.py
+++ homeassistant/components/youtube/application_credentials.py
@@ -3,7 +3,7 @@ from homeassistant.components.application_credentials import AuthorizationServer
 from homeassistant.core import HomeAssistant
 
 
-async def async_get_authorization_server(hass: HomeAssistant) -> AuthorizationServer:
+async def async_get_authorization_server() -> AuthorizationServer:
     """Return authorization server."""
     return AuthorizationServer(
         "https://accounts.google.com/o/oauth2/v2/auth",
